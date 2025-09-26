bbba62c1ae5ebb2d041eaaffe22607014a1ebe45
이지원 <168502753+Vryez11@users.noreply.github.com>
2025-09-27 08:09:45 +0900

    study: Java 입문 복습
    - 1. hello java

---
+++ src/main/java/com/study/basic/HelloJava.java
@@ -0,0 +1,11 @@
+package main.java.com.study.basic;
+
+/*
+이것은 주석입니다.
+ */
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
+
