2691f3334d753b209291b8f946161fd21d90b64a
이지원 <168502753+Vryez11@users.noreply.github.com>
2025-09-29 10:25:37 +0900

    feat: 배열 문제 풀이 추가
    - 상품 관리 프로그램 문제 풀이

--- src/main/java/com/study/basic/array/Array.java
+++ src/main/java/com/study/basic/array/Array.java
@@ -1,5 +1,7 @@
 package main.java.com.study.basic.array;
 
+import java.util.Scanner;
+
 public class Array {
 
     public static void main(String[] args) {
@@ -10,7 +12,42 @@ public class Array {
         int[] scores = {90, 80, 70, 60, 50};
 
         for (int score : scores) {
-            System.out.println("score = " + score);
+            //System.out.println("score = " + score);
+        }
+
+        // problem
+        Scanner input = new Scanner(System.in);
+
+        String[] productNames = new String[3];
+        int[] productPrices = new int[3];
+        int productCount = 0;
+
+        while (true) {
+            System.out.println("1. 상품 등록 | 2. 상품 목록 | 3. 종료");
+            System.out.print("메뉴를 선택하세요: ");
+
+            int menu = input.nextInt();
+
+            if (menu == 3) break;
+            if (menu == 2) {
+                for(int i = 0; i < productCount; i++) {
+                    System.out.println(productNames[i] + ": " + productPrices[i] + "원");
+                }
+                continue;
+            }
+
+            if (productCount > 2) {
+                System.out.println("메뉴가 가득차 더 이상 추가할 수 없습니다.");
+                continue;
+            }
+
+            System.out.print("상품 이름을 입력하세요: ");
+            String productName = input.next();
+            System.out.print("상품 가격을 압력하세요: ");
+            int productPrice = input.nextInt();
+
+            productNames[productCount] = productName;
+            productPrices[productCount++] = productPrice;
         }
     }
 }
