60e5fcdf4c447740c8e481928af7c43170ab7b3b
이지원 <168502753+Vryez11@users.noreply.github.com>
2026-01-21 19:23:14 +0900

    feat: 스레드 join 메서드 문제 풀이
    - join 대기 시간을 계산하고 이에 맞게 알고리즘 변경 문제 풀이

---
+++ src/main/java/study/adv1/thread/control/test/JoinTestMain.java
@@ -0,0 +1,30 @@
+package main.java.study.adv1.thread.control.test;
+
+import static main.java.study.adv1.thread.MyLogger.log;
+import static main.java.study.adv1.thread.ThreadUtils.sleep;
+
+public class JoinTestMain {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(new MyTask(), "t1");
+        Thread t2 = new Thread(new MyTask(), "t2");
+        Thread t3 = new Thread(new MyTask(), "t3");
+        t1.start();
+        t2.start();
+        t3.start();
+        
+        t1.join();
+        t2.join();
+        t3.join();
+        System.out.println("모든 스레드 실행 완료");
+    }
+    static class MyTask implements Runnable {
+        @Override
+        public void run() {
+            for (int i = 1; i <= 3; i++) {
+                log(i);
+                sleep(1000);
+            }
+        }
+    }
+}
