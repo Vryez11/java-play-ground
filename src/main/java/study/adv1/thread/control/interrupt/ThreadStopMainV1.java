d06d005dea10680af3738eee53d595f69ba3177b
이지원 <168502753+Vryez11@users.noreply.github.com>
2026-01-21 19:44:44 +0900

    feat: 스레드 인터럽트 실습 예제
    - 인터럽트를 통해 대기 상태 탈출 예제
    - Thread.isInterrupted 와 Thread.interrupted 차이 실습 예제

---
+++ src/main/java/study/adv1/thread/control/interrupt/ThreadStopMainV1.java
@@ -0,0 +1,42 @@
+package main.java.study.adv1.thread.control.interrupt;
+
+import static main.java.study.adv1.thread.MyLogger.log;
+import static main.java.study.adv1.thread.ThreadUtils.sleep;
+
+public class ThreadStopMainV1 {
+
+    public static void main(String[] args) {
+
+        MyTask task = new MyTask();
+        Thread thread = new Thread(task, "work");
+        thread.start();
+
+        sleep(100);
+        log("작원 중단 지시 thread.interrupt()");
+        thread.interrupt();
+        log("work 스레드 인터럽트 상태1 = " + thread.isInterrupted());
+    }
+
+    static class MyTask implements Runnable {
+
+        @Override
+        public void run() {
+            while (!Thread.currentThread().isInterrupted()) {
+                log("작업 중");
+            }
+            log("work 스레드 인터럽트 상태2 = " +
+                    Thread.currentThread().isInterrupted());
+            try {
+                log("자원 정리 시도");
+                Thread.sleep(1000);
+                log("자원 정리 완료");
+            }
+            catch (InterruptedException e) {
+                log("자원 정리 실패 - 자원 정리 중 인터럽트 발생");
+                log("work 스레드 인터럽트 상태3 = " +
+                        Thread.currentThread().isInterrupted());
+            }
+            log("작업 종료");
+        }
+    }
+}
